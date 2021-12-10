f1768aa9d6f7d4cc1aa933054fbf34dfae85d83e
Francis Leclerc <leclerc.francis@googlemail.com>
2021-12-10 08:49:48 -0500

    logging

--- db.py
+++ db.py
@@ -8,22 +8,3 @@ client = pymongo.MongoClient(conn)
 db = client.classDB
 
 dataset = db.binancetest
-
-# db.classroom.insert_many(
-#     [
-#       {
-#         'name': 'Jerry',
-#         'hobbies': 'gardening',
-#         'classroom_teacher': "Jerry's Dad",
-#         'exam_scores': [1, 0, -25]
-#       },
-#       {
-#         'name': 'Muhammed',
-#         'hobbies': {'exercise': ['swimming', 'running'],
-#                     'games': 'chess'},
-#         'classroom_teacher': 'Jerry',
-#         'exam_scores': [100, 100, 100]
-#       }
-#     ])
-# for x in db.classroom.find():
-#   print(x)
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+import logging.config
+
 from time import sleep
 from binance_f import RequestClient
 from binance_f.constant.test import *
@@ -5,17 +8,28 @@ from binance_f.base.printobject import *
 from datetime import datetime
 from db import dataset
 
-request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 
+
+def setupLogging():
+    import yaml
+    from yaml import Loader, Dumper
+    cfg = yaml.load(open('logging.yml').read(), Loader=Loader)
+    logging.config.dictConfig(cfg)
+
+
+request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 def saveLongShort():
     glob = request_client.get_global_long_short_accounts(symbol="BTCUSDT", period='5m', limit=1)
     top = request_client.get_top_long_short_accounts(symbol="BTCUSDT", period='5m', limit=1)
-    print(datetime.fromtimestamp(glob[0]['timestamp']/1000.0))
+    logger.info(f"saveLongShort {datetime.fromtimestamp(glob[0]['timestamp']/1000.0)}")
     dataset.insert_one({'global': glob, 'top': top})
 
 
+setupLogging()
+logger = logging.getLogger('pybin.main')
+logger.info(f'info {logger.getEffectiveLevel()}')
 import schedule
-schedule.every(5).minutes.do(saveLongShort)
+schedule.every(5).seconds.do(saveLongShort)
 
 while True:
     schedule.run_pending()
