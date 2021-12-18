44bbd1a636500875c80eea35919653c1e7d7fc60
Francis Leclerc <leclerc.francis@googlemail.com>
2021-12-18 15:45:27 -0500

    graph

--- db_setup.py
+++ db_setup.py
@@ -4,3 +4,8 @@ from db import db
 db.longshort.create_index( [("timestamp", 1)] )
 #%%
 db.depth.create_index( [("E", 1)] )
+
+#%%
+db.trades.create_index( [("eventTime", 1)] )
+
+# %%
--- dbbook.py
+++ dbbook.py
@@ -11,6 +11,54 @@ print(db.depth.count_documents({}))
 print(db.trades.count_documents({}))
 print(db.longshort.count_documents({}))
 
+# %%
+db.createCollection(
+    "tradests",
+  {
+       "timeseries": {
+          "timeField": "eventTime",
+       }
+    }
+)
+
+# %%
+for entry in db.trades.find():
+    entry["eventTime"] = datetime.fromtimestamp(entry["eventTime"]/1000.0)
+    db.tradests.insert_one(entry)
+
+# %%
+pipeline = [
+    { "$sort" : { "eventTime" : 1 } },
+    {"$addFields": {
+       "t": "$eventTime",
+       "dt": { "$toDate": "$eventTime" },
+      "date": {"$dateToParts": {"date": { "$toDate": { "$toDate": "$eventTime" } } } }
+       }
+   }]
+# %%
+pipeline.append(
+   {
+      "$group": {
+         "_id": {
+            "date": {
+               "year": "$date.year",
+               "month": "$date.month",
+               "day": "$date.day",
+               "hour": "$date.hour",
+               "minute": "$date.minute",
+            }
+         },
+         "minPrc": { "$min": "$price" },
+         "maxPrc": { "$max": "$price" }
+      }
+   }
+)
+
+# %%
+for entry in db.trades.aggregate(pipeline):
+    print (entry)
+    
+
 # %%  print depth
 for entry in db.depth.find():
     print(entry)
@@ -48,10 +96,12 @@ print (f"global min: {min(gdiff)} max: {max(gdiff)}   top min: {min(tdiff)} max:
 # %%
 import pandas as pd
 data = pd.json_normalize(list(db.longshort.find().sort( [( "timestamp", 1 )] )))
+data2 = pd.json_normalize(list(db.trades.find()))
 #'global',
 #['timestamp', 'longShortRatio']
 #)
 #print(data.head())
+pd.date_range
 df = pd.DataFrame(data)
 df['global.longShortRatio'] = pd.to_numeric(df['global.longShortRatio'],errors='coerce')
 df['top.longShortRatio'] = pd.to_numeric(df['top.longShortRatio'],errors='coerce')
